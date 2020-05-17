90b4fbe8c164740eeb554fe566564b7c3cf02b47
olliebrownlow <olliebrownlow@gmail.com>
2020-05-18 00:07:35 +0100

    adds full login/logout capability with auth0

--- components/layout.js
+++ components/layout.js
@@ -2,11 +2,14 @@ import Head from "next/head";
 import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
+import { UserProvider, useFetchUser } from "../utils/user";
+import Navbar from "./navbar";
 
 const name = "Ollie Brownlow";
 export const siteTitle = "Next.js Sample Website";
 
 export default function Layout({ children, home }) {
+  const { user, loading } = useFetchUser();
   return (
     <div className={styles.container}>
       <Head>
@@ -24,43 +27,46 @@ export default function Layout({ children, home }) {
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
-      <header className={styles.header}>
-        {home ? (
-          <>
-            <img
-              src="/images/profile.png"
-              className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
-              alt={name}
-            />
-            <h1 className={utilStyles.heading2Xl}>{name}</h1>
-          </>
-        ) : (
-          <>
-            <Link href="/">
-              <a>
-                <img
-                  src="/images/profile.png"
-                  className={`${styles.headerImage} ${utilStyles.borderCircle}`}
-                  alt={name}
-                />
-              </a>
-            </Link>
-            <h2 className={utilStyles.headingLg}>
+      <UserProvider value={{ user, loading }}>
+        <Navbar />
+        <header className={styles.header}>
+          {home ? (
+            <>
+              <img
+                src="/images/profile.png"
+                className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
+                alt={name}
+              />
+              <h1 className={utilStyles.heading2Xl}>{name}</h1>
+            </>
+          ) : (
+            <>
               <Link href="/">
-                <a className={utilStyles.colorInherit}>{name}</a>
+                <a>
+                  <img
+                    src="/images/profile.png"
+                    className={`${styles.headerImage} ${utilStyles.borderCircle}`}
+                    alt={name}
+                  />
+                </a>
               </Link>
-            </h2>
-          </>
+              <h2 className={utilStyles.headingLg}>
+                <Link href="/">
+                  <a className={utilStyles.colorInherit}>{name}</a>
+                </Link>
+              </h2>
+            </>
+          )}
+        </header>
+        <main>{children}</main>
+        {!home && (
+          <div className={styles.backToHome}>
+            <Link href="/">
+              <a>← Back to home</a>
+            </Link>
+          </div>
         )}
-      </header>
-      <main>{children}</main>
-      {!home && (
-        <div className={styles.backToHome}>
-          <Link href="/">
-            <a>← Back to home</a>
-          </Link>
-        </div>
-      )}
+      </UserProvider>
     </div>
   );
 }
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -3,8 +3,21 @@ import { getAllPostIds, getPostData } from "../../lib/posts";
 import Head from "next/head";
 import Date from "../../components/date";
 import utilStyles from "../../styles/utils.module.css";
+import { useFetchUser } from "../../utils/user";
+import Router from "next/router";
 
 export default function Post({ postData }) {
+  const { user, loading } = useFetchUser();
+  if (loading) {
+    return (
+      <Layout>
+        <p>Loading...</p>;
+      </Layout>
+    );
+  }
+  if (!user && !loading) {
+    Router.replace("/");
+  }
   return (
     <Layout>
       <Head>
